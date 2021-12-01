05d2e0e2aef372ce1e18edf793cb21717ed93c6a
Sergio Alberto Luis Cano <serluis@ucm.es>
2021-12-01 09:38:08 +0100

    readtxt cambiado de sitio y puestas lineas en gamemanager

--- FlowFree/Assets/Scripts/Logic/Map.cs
+++ FlowFree/Assets/Scripts/Logic/Map.cs
@@ -69,7 +69,7 @@ namespace Flow.Logic
                 Debug.Log(i+" "+header[i]);
             // x.Parse("9:11+B,0,1,6,,45:46:47:48:50:51:52:53;79,88,87,86,85,84,83,82,73,64,65,66,67,68,59,60,61,62,71;11,12,13,14,15,16,25,34,33,32,31,30,39,38,37,36,27,18,9;24,23,22,21,20,29;0,1,10,19,28;2,3,4,5,6,7,8,17,26,35,44,43,42,41,40,49,58,57,56,55,54,63,72,81,90,91,92,93,94,95,96,97,98,89,80;70,69,78,77,76,75,74");
             }*/
-            if (header[5] != "")
+            if (header.Length > 5 && header[5] != "")
             {
                 string [] vaciosS = header[5].Split(':');//OJO PUEDE HABER CARACTERES RAROS
 
@@ -134,3 +134,20 @@ namespace Flow.Logic
        
     }
 }
+
+//hasta el 14 son 4 digitos, tambien el w1-1 = dimension,0,levelnumber,tuberiasNumber;
+//el 15 tiene 4 digitos e incluye ':' para muros? = dimension,b,levelnumber,tuberiasNumber; 25:12,...
+//el 16 al 22,32 = dimension,0,levelnumber,tuberiasNumber;
+//el 23 tiene X, 4 numeros y un hueco entre comas = x_dimension,0,levelnumber,tuberiasNumber,,y serie de dobles puntos;15:25:12:5 etc
+//el 24,29,30,38,40 tiene dimension diferente dimension:dimension,0,levelnumber,tuberiasnumber;
+//el 25 mania vuelve a tener 4 = dimension,0,levelnumber,tuberiasNumber;
+//el 26,31 tiene 4 = dimension,0,levelnumber,tuberiasNumber,,,y la serie del tipo 2|9:1|8:7 etc
+//el 27 tiene W, y ':' W_6:3,0,1,4,, (pueden ser 2 o 3 comas) 0_0:4_0:5_0:12_0:16_0:17_0,3|15:2|14:1|13;
+//el 28,33,43 es una fiesta ke tiene de todo tienen en comun que son 4 digitos y luego dos comas 
+//el 34,36 tiene el primer numero de forma rara = 9:11+B y luego 3 mas con dos comas y seccion de ':'
+//el 35 tiene dimension+B:I,0,levelnumber,tuberiasNumber,,y casillas 81_0B:93_0B: etc
+//el 36,39 jumbo 4 digitos, tres comas y 1|12:2|etc
+//el 37 igual que el 36 pero se sale de una linea
+//levelpack_d1 cambia el 0 del segundo digito por otro numero
+//w1-14 = 11+B,0,11,9,,0_0B:10_0B:110_0B:120_0B,1|12:9|20:11|
+//No se observan mas variaciones
